abef1bec008b29288812cd629859c98cb2e7026a
Lhar Gil <devlana@outlook.com>
2019-09-02 05:49:30 +0800

    refactor: enabled main to access scoped services

--- src/SplitThatBill.Backend.API/Program.cs
+++ src/SplitThatBill.Backend.API/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SplitThatBill.Backend.Data;
 
 namespace SplitThatBill.Backend.API
 {
@@ -14,7 +17,26 @@ namespace SplitThatBill.Backend.API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var services = scope.ServiceProvider;
+                    var context = services.GetService<SplitThatBillContext>();
+
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError("An error has occurred while migrating the database.", ex);
+                }
+            }
+            
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
