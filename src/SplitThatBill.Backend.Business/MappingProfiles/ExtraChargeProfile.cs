b4c2a35855a37fd7e3e9c8c306f65ea2896589e6
Lhar Gil <devlana@outlook.com>
2019-10-02 23:24:20 +0800

    feat: added extra charge mapping

---
+++ src/SplitThatBill.Backend.Business/MappingProfiles/ExtraChargeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SplitThatBill.Backend.Business.Dtos;
+using SplitThatBill.Backend.Core.OwnedEntities;
+
+namespace SplitThatBill.Backend.Business.MappingProfiles
+{
+    public class ExtraChargeProfile : Profile
+    {
+        public ExtraChargeProfile()
+        {
+            CreateMap<ExtraCharge, ExtraChargeDto>();
+        }
+    }
+}
